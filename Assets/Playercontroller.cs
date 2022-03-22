760890cf29ef414708c1fd6e71a590ce68503369
Theo zibell <theo@zibell.se>
2022-03-22 14:53:27 +0100

    börjar med att begränsa speeden

--- Assets/Playercontroller.cs
+++ Assets/Playercontroller.cs
@@ -5,28 +5,17 @@ using UnityEngine;
 public class Playercontroller : MonoBehaviour
 {
     Rigidbody2D rb2D;
-    float speed;
-    float jumpforce;
-    bool isJump;
-    bool isGrund;
-    public Vector2 position = new Vector2();
+    public float speed = 1;
+    public float jumpforce = 10;
+    bool isJump = false;
+    bool isGrund = true;
 
     float moveHorizontal;
-
-    public int hp = 100;
-    public int dmg = 20;
-    bool isAlive = true;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2D = gameObject.GetComponent<Rigidbody2D>();
-
-
-        speed = 3;
-        jumpforce = 5;
-        isJump = false;
-        isGrund = true;
     }
 
     // Update is called once per frame
@@ -37,23 +26,32 @@ public class Playercontroller : MonoBehaviour
         {
             if (isGrund == true)
             {
+                Debug.Log("j");
                 isJump = true;
             }
         }
     }
     void FixedUpdate()
     {
-        if (moveHorizontal > 0.1 || moveHorizontal < 0.1)
+        if (moveHorizontal > 0 || moveHorizontal < 0)
         {
-            // rb2D.AddForce(new Vector2(moveHorizontal * speed, 0f), ForceMode2D.Impulse);
-            rb2D.velocity = new Vector2(moveHorizontal * speed, rb2D.velocity.y);
+            Debug.Log(rb2D.velocity);
+            rb2D.AddForce(new Vector2(moveHorizontal * speed, 0), ForceMode2D.Impulse);
+            //rb2D.velocity = new Vector2(moveHorizontal * speed, rb2D.velocity.y);
+
         }
 
         if (isGrund == true && isJump == true)
         {
-            //rb2D.AddForce(new Vector2(0f, jumpforce), ForceMode2D.Impulse);
-            rb2D.velocity = new Vector2(rb2D.velocity.x, jumpforce);
+            rb2D.AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
+            //rb2D.velocity = new Vector2(rb2D.velocity.x, jumpforce);
             isJump = false;
+        }
+
+        if (rb2D.velocity.x >= 5)
+        {
+            Debug.Log("yes");
+            rb2D.AddForce(new Vector2(moveHorizontal * -speed, 0), ForceMode2D.Impulse);
         }
 
     }
